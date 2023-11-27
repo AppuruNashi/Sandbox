cd3dd5f38fa891beefa34276a88b7536c9822daa
Leila <essafi_l@outlook.com>
2023-11-26 21:48:32 -0500

    do not push

--- EngineSetup/BatchFiles/Build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo Start building project ...
-
-call %~dp0vars.bat
-
-call "%BUILD_BAT%" %PROJECT%Editor Win64 Development "%UPROJECT_PATH%" -waitmutex -NotHotReload
-
-echo Project builded !
